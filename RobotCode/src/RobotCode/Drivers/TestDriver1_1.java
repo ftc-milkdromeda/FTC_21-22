ce88b8133c05aac8def4e9797a870069713877e5
Tyler Wang <wangtyler123@gmail.com>
2021-07-19 10:07:02 -0500

    test: changed some test files.
    TEST CODE

--- RobotCode/src/RobotCode/Drivers/TestDriver1_1.java
+++ RobotCode/src/RobotCode/Drivers/TestDriver1_1.java
@@ -1,6 +1,8 @@
 package RobotCode.Drivers;
 
 import Framework.Drivers.Driver;
+import Framework.Tasks.Task;
+import Framework.Tasks.TaskError;
 
 public class TestDriver1_1 extends Driver {
     public TestDriver1_1() {
@@ -9,8 +11,8 @@ public class TestDriver1_1 extends Driver {
 
     private static DriverDirectory driverType = DriverDirectory.TEST_DRIVER_1;
 
-    public void doSomething() {
-        if(!super.isActive())
+    public void doSomething(Task task) {
+        if(super.validateCall(task) != TaskError.NO_ERROR)
             return;
         System.out.println("TestDriver1 did nothing.");
     }
