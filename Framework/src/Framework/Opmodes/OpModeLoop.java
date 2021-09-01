1ed5f1e807a7d173a4a17d9797a6609eeea87e10
Tyler Wang <wangtyler123@gmail.com>
2021-09-01 07:15:02 -0500

    feat: added refresh cap for OpModeLoop.
    MINOR CHANGE

--- Framework/src/Framework/Opmodes/OpModeLoop.java
+++ Framework/src/Framework/Opmodes/OpModeLoop.java
@@ -7,20 +7,36 @@ import Framework.GeneralError;
 public abstract class OpModeLoop extends OpMode{
     public OpModeLoop(DriverManager driverManager) {
         super(driverManager);
+
+        this.maximumRefresh = -1;
+    }
+    public OpModeLoop(DriverManager driverManager, double maximumRefresh) {
+        super(driverManager);
+
+        this.maximumRefresh = maximumRefresh;
     }
 
     @Override
     protected Error opMode() {
         do {
+            long startTime = System.currentTimeMillis();
             Error output = this.loop();
+
             if (output != GeneralError.NO_ERROR)
                 return output;
+
+            if(this.maximumRefresh == -1)
+                continue;
+
+            while(super.opModeIsActive() && System.currentTimeMillis() - startTime < (long)(1000 / this.maximumRefresh));
         } while(super.opModeIsActive());
 
         return GeneralError.NO_ERROR;
     }
 
-    public abstract Error loop();
+    protected abstract Error loop();
+
+    private double maximumRefresh;
 }
 
 //todo add opmode refresh monitor (FUTURE CHANGE)
